e382ef86433517489c146cc7a5cf52ca8e58204e
Mark Macaraeg <markmcrg9@gmail.com>
2023-08-27 22:50:35 +0800

    Fixed caching of employee details

--- pages/weatherAlerts.py
+++ pages/weatherAlerts.py
@@ -43,21 +43,18 @@ def get_cities_data():
 
 cities_data = get_cities_data()
 
-@st.cache_data
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit", "/export?format=csv")
     return pd.read_csv(csv_url)
 
 gsheets_url = st.secrets["public_gsheets_url"]
-df = load_data(gsheets_url)
+
 
 def get_coordinates(city_name):
     for city_data in cities_data:
         if city_data['city_name'] == city_name:
             return city_data['latitude'], city_data['longitude']
     return None, None  # Return None if city name not found in the list
-st.sidebar.markdown(f"[**Employee Database**]({gsheets_url})")
-st.sidebar.dataframe(df, hide_index=True)
 
 def fetch_weather_alerts(lat, lon):
         url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely,daily&appid=63cab36178a3d987d900af49583e506d"
@@ -70,19 +67,20 @@ def fetch_weather_alerts(lat, lon):
 def determine_status(alerts):
     return 'Alert' if alerts else 'No Alert'
 
-tab1, tab2 = st.tabs(["⚠️ Weather Alerts", " 📰 Weather News"])
+st.title('⚠️ Weather Alerts')
+st.markdown(f"[**Employee Database**]({gsheets_url})")
+fetch_alerts = st.button('Fetch Weather Alerts', key="fetch_weather_alerts")
+st.info("This app fetches weather alerts displays them in a table.")
 
-fetch_alerts = st.sidebar.button('Fetch Weather Alerts', key="fetch_weather_alerts")
-
-with tab1:
-    if fetch_alerts:
-        if api_key:
-            with st.spinner("Fetching Weather Alerts..."):
-                df['Latitude'], df['Longitude'] = zip(*df['city'].map(get_coordinates))
-                df['status'] = df.apply(lambda row: determine_status(fetch_weather_alerts(row['Latitude'], row['Longitude'])), axis=1)
-            st.dataframe(df, hide_index=True)
-        else:
-            st.error("Please enter your API key.")
+if fetch_alerts:
+    if api_key:
+        df = load_data(gsheets_url)
+        with st.spinner("Fetching Weather Alerts..."):
+            df['Latitude'], df['Longitude'] = zip(*df['city'].map(get_coordinates))
+            df['status'] = df.apply(lambda row: determine_status(fetch_weather_alerts(row['Latitude'], row['Longitude'])), axis=1)
+        st.dataframe(df, hide_index=True)
+    else:
+        st.error("Please enter your API key.")
 
 # Latest Philippine Weather News with date
 url = "https://www.rappler.com/nation/weather"
@@ -102,18 +100,3 @@ for i, url in enumerate(article_urls):
         article_content.append(article.text)
         
 news_summaries = []
-with tab2:
-    if fetch_alerts:
-        st.subheader("Latest Philippine Weather News")
-        if article_title and article_content:
-            for i, content in enumerate(article_content):
-                with st.spinner("Generating summary..."):
-                    summary = generate_summary(article_content[i])
-                    news_summaries.append(summary)
-            col1, col2, = st.columns(2, gap="medium")
-            with col1:
-                st.markdown(f"#### [{article_title[0]}]({article_urls[0]})")
-                st.success(news_summaries[0])
-            with col2:
-                st.markdown(f"#### [{article_title[1]}]({article_urls[1]})")
-                st.success(news_summaries[1])
