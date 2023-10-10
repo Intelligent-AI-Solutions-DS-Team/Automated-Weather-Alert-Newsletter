a3a6479453bfaa78be95caa711f2390d5be63552
Mark Macaraeg <markmcrg9@gmail.com>
2023-10-10 13:38:44 +0800

    Used Hugchat instead of SerpAPI for web search feature

--- pages/talking_points.py
+++ pages/talking_points.py
@@ -2,44 +2,47 @@ from langchain.agents import load_tools, initialize_agent
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 from email.message import EmailMessage
+from hugchat import hugchat
+from hugchat.login import Login
 import smtplib
 import time
 
 openai_api_key = st.secrets['openai_api_key']
-serpapi_api_key = st.secrets['serpapi_key']
+
+# secrets.toml
 EMAIL_ADDRESS = 'hello@markmcrg.com'
 EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
+
+passwd = st.secrets['huggingface_password']
+email = 'contact@markmcrg.com'
+
+cookie_token = st.secrets['cookie_token']
+cookie_hf_chat = st.secrets['cookie_hf_chat']
 
 st.title("🗞️ Talking Points Generator")
 topic = st.text_input ("Enter your topic here")
 
-llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0.6, max_tokens=1024, model_name="gpt-3.5-turbo")
-
-tool_names = ['serpapi']
-
-tools = load_tools (tool_names, serpapi_api_key=serpapi_api_key)
-
-agent = initialize_agent(tools, 
-                          llm, 
-                          agent='chat-zero-shot-react-description', 
-                          verbose=True,
-                          handle_parsing_errors=True,
-                          early_stopping_method="generate")
-
 user_email = st.text_input("Email Address", key="user_email", value="contact@markmcrg.com")
 generate_button = st.button("Generate and Send Email")
-# prompt = f"Please generate three categories related to the topic of {topic}. For each category, provide five bulleted talking points starting with an emoji. The talking points should focus on the most recent news and updates about {topic}. Finally, conclude with a general summary of the up-to-date information."
-prompt = f"Generate five bulleted talking points starting with an emoji. The bulleted talking points should focus on the most recent news and updates about {topic}. "
+prompt = f"Please generate three categories related to the topic of {topic}. For each category, provide five bulleted talking points starting with an emoji. The talking points should focus on the most recent news and updates about {topic}. Finally, conclude with a general summary of the up-to-date information."
+# prompt = f"Generate five bulleted talking points starting with an emoji. The bulleted talking points should focus on the most recent news and updates about {topic}. "
 
 if generate_button and user_email:
     with st.spinner('Generating...'):
-        response = agent.run(prompt)
+        sign = Login(email, passwd)
+        cookies = sign.login()
+        cookie_path_dir = "./cookies_snapshot"
+        cookies = {"token": cookie_token, "hf-chat": cookie_hf_chat}
+        chatbot = hugchat.ChatBot(cookies=cookies)
+
+        response = chatbot.query(prompt, web_search=True)
         st.write(response)
     with st.spinner("Sending email..."):
         msg = EmailMessage()
         msg['Subject'] = f'Talking points about {topic}'
         msg['From'] = EMAIL_ADDRESS
         msg['To'] = user_email
+        response = str(response)
         msg.set_content(response)
         for attempt in range(1, 4):
             try:
