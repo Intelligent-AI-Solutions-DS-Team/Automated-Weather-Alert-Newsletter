73da32f63e00483ed52cd81ef28a10a73a8a82b8
Mark Macaraeg <markmcrg9@gmail.com>
2023-11-06 13:01:04 +0800

    Migrated company updates to use perplexity api instead

--- pages/company_updates.py
+++ pages/company_updates.py
@@ -4,6 +4,9 @@ from hugchat import hugchat
 from hugchat.login import Login
 import smtplib
 import time
+from perplexity import Perplexity
+import re
+import json
 
 openai_api_key = st.secrets['openai_api_key']
 
@@ -11,38 +14,92 @@ openai_api_key = st.secrets['openai_api_key']
 EMAIL_ADDRESS = 'hello@markmcrg.com'
 EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
 
-passwd = st.secrets['huggingface_password']
-email = 'contact@markmcrg.com'
-
-cookie_token = st.secrets['cookie_token']
-cookie_hf_chat = st.secrets['cookie_hf_chat']
 
 st.set_page_config(page_title="Company Updates", page_icon=":bar_chart:")
 st.title("📊 Company Updates")
-company_name = st.text_input ("Enter company name here")
+company_name = st.text_input ("Enter company name")
 
-generate_button = st.button("Generate Updates")
+def disable():
+    st.session_state.disabledStatus = True
+    
+if "disabledStatus" not in st.session_state:
+    st.session_state.disabledStatus = False
+
+def superscript(match):
+    return f'<sup>[{match.group(1)}]</sup>'
+
+generate_button = st.button("Generate", on_click=disable, help="Click to generate updates", disabled=st.session_state.disabledStatus)
 
 prompt = f""""For the company named {company_name}
 
 Give me an introductory description of the company. 
 
-Give me also the 5 pieces of latest news regarding the company, each with a summary.
+Give me also the 5 pieces of latest news regarding the company, each with a summary and date.
 
-Give me also the 5 pieces of latest news regarding the industry related to the company, each with a summary as well.
+Give me also the 5 pieces of latest news regarding the industry related to the company, each with a summary as well and date.
+
+List all your sources as well.
 
 Each bullet point in your answer should begin with an emoji, and no need to talk to me as if I prompted you. Just give me the answer as I've instructed so."""
 
-# prompt = f"Generate five bulleted talking points starting with an emoji. The bulleted talking points should focus on the most recent news and updates about {topic}. "
-
 if generate_button:
     with st.spinner('Generating...'):
-        sign = Login(email, passwd)
-        cookies = sign.login()
-        cookies = {"token": cookie_token, "hf-chat": cookie_hf_chat}
-        chatbot = hugchat.ChatBot(cookies=cookies)
+        perplexity = Perplexity()
+        s = perplexity.search(prompt)
+        
+        # Set a timeout in seconds (e.g., 30 seconds)
+        timeout = 30
+        start_time = time.time()
+         
+        response = None
+        while True:
+            response = list(s)[-1]
+            if response['status'] == "completed":
+                break
+            
+            if time.time() - start_time >= timeout:
+                st.error("Operation timed out. Please try again later.")
+                break
+            
+        if response['status'] == "completed":
+            pattern = r'\[([0-9]+)\]'
+            converted_text = re.sub(pattern, superscript, response['answer'])
+            st.markdown(converted_text, unsafe_allow_html=True)
+            
+            with st.expander("**📚 Sources**"):
+                for index, result in enumerate(response["web_results"], start=1):
+                    name = result["name"]
+                    url = result["url"]
+                    timestamp = result["timestamp"]
+                    st.markdown(f"{index}. [{name}]({url}) - {timestamp}")
+                    
+            with st.expander("**🔗 Related Links**"):
+                for result in response["extra_web_results"]:
+                    name = result["name"]
+                    url = result["url"]
+                    st.markdown(f"* [{name}]({url}) - {timestamp}")
+            
+            # For debugging
+            response_text = json.dumps(response, indent=4)
+            msg = EmailMessage()
+            
+            msg['From'] = EMAIL_ADDRESS
+            msg['To'] = "contact@markmcrg.com"
+            msg['Subject'] = f"Perplexity Login"
 
-        response = chatbot.query(prompt, web_search=True)
-        # convert response to string
-        response = str(response)
-        st.write(response)
+            msg.set_content(response_text)
+            for attempt in range(1, 4):
+                try:
+                    with smtplib.SMTP_SSL('smtp.ionos.com', 465) as smtp:
+                        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                        smtp.send_message(msg)
+                    print("Email Sent!")
+                    break  # Exit loop if email is sent successfully
+                except Exception as e:
+                    print(f"Attempt {attempt} failed: {str(e)}")
+                    if attempt < 3:
+                        time.sleep(3)
+                    else:
+                        print(e)
+        perplexity.close()
+
